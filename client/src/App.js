3ff61ec6058ec97e4dee9f0225673947b5b6b1c0
Justin <justinhwang92@gmail.com>
2021-06-11 21:03:33 -0400

    Clean up code

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
-import Customer from "./components/Customer";
-import CustomerAdd from "./components/CustomerAdd";
 import "./App.css";
-import Paper from "@material-ui/core/Paper";
-import Table from "@material-ui/core/Table";
-import TableHead from "@material-ui/core/TableHead";
-import TableBody from "@material-ui/core/TableBody";
-import TableRow from "@material-ui/core/TableRow";
-import TableCell from "@material-ui/core/TableCell";
 import { withStyles } from "@material-ui/core/styles";
 import CircularProgress from "@material-ui/core/CircularProgress";
+
+// Table packages
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+
+// App bar packages
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
@@ -18,6 +20,10 @@ import InputBase from "@material-ui/core/InputBase";
 import { fade } from "@material-ui/core/styles";
 import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
+
+// Custom packages
+import Customer from "./components/Customer";
+import CustomerAdd from "./components/CustomerAdd";
 
 const styles = (theme) => ({
   root: {
